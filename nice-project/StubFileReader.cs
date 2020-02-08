4778d1881555c0223f62acdfab457bf3e145a79c
g.isachenko <george.isachenko@tatemgames.com>
2020-02-08 18:16:23 +0200

    Remove stub file reader

--- nice-project/StubFileReader.cs
+++ nice-project/StubFileReader.cs
@@ -7,7 +7,7 @@ namespace NiceProject
     {
         public IEnumerable<int> ReadAllNumbers()
         {
-            return new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            return new[] {1, 2, 3, 4, 5, 6};
         }
     }
 }
